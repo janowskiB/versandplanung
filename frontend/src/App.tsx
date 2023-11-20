1008610b0e0cee1c60052d165931c55981ce698a
florianludwig <fludwig@kpler.com>
2023-11-20 12:07:47 +0100

    Created Header and Pages Links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
-import CustomerList from "./components/CustomerList";
-import EmployeeList from "./components/EmployeeList";
-import FleetList from "./components/FleetList";
+import CustomerList from "./pages/CustomerList";
+import EmployeeList from "./pages/EmployeeList";
+import FleetList from "./pages/FleetList";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
         <BrowserRouter>
             <Header/>
             <Routes>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/customers" element={<CustomerList/>}/>
                 <Route path="/employees" element={<EmployeeList/>}/>
                 <Route path="/fleets" element={<FleetList/>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     );
